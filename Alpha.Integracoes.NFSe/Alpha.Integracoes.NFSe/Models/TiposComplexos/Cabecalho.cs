dad732658558fd8a9846a5575569680b9faa7565
Igor Bueno <igor@alphasoftware.com.br>
2020-09-21 15:53:36 -0400

    Criando cabeçalho

---
+++ Alpha.Integracoes.NFSe/Alpha.Integracoes.NFSe/Models/TiposComplexos/Cabecalho.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Xml.Serialization;
+
+namespace Alpha.Integracoes.NFSe.Models.TiposComplexos.Cabecalho
+{
+	[XmlRoot(ElementName = "cabecalho", Namespace = "http://www.abrasf.org.br/nfse")]
+	public class Cabecalho
+	{
+		[XmlAttribute(AttributeName = "versao", Namespace = "http://www.abrasf.org.br/nfse")]
+		public string Numero { get; set; }
+		[XmlElement(ElementName = "versaoDados", Namespace = "http://www.abrasf.org.br/nfse")]
+		public string VersaoDados { get; set; }
+	}
+}
